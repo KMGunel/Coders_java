0a4d051d3755378ed48bda68102b7ba8f84f52de
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-05-21 04:40:36 +0400

    sade ve mürekkeb eded

---
+++ src/main/java/exercise/Others/SimpleOrComplexDigit.java
@@ -0,0 +1,25 @@
+package main.java.exercise.Others;
+
+import java.util.Scanner;
+
+public class SimpleOrComplexDigit {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int m=0;
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+                m++;
+            }
+        }
+        if (n==0){
+            System.out.println("Daxil edilən ədəd 0-dır.");
+        }
+        else if (m>2){
+            System.out.println("Daxil edilən MÜRƏKKƏB ədəddir.");
+        }
+        else{
+            System.out.println("Daxil edilən ədəd SADƏ ədəddir.");
+        }
+    }
+}
