0948310ac5c90df8b189ea34f80700a94c64bd64
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-05-22 16:53:22 +0400

    smallest divisor

--- src/main/java/exercise/others/Divisors.java
+++ src/main/java/exercise/others/Divisors.java
@@ -6,11 +6,17 @@ public class Divisors {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) {
-                 System.out.printf("%d %d ",i,n/i);
-            }
 
+        for (int i = 1; i < (int)Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                 System.out.printf("%d ",i);
+            }
+        }
+        for (int i = (int)Math.sqrt(n); i >=1 ; i--) {
+            if (n % i == 0) {
+                System.out.printf("%d ",n/i);
+            }
         }
 
-    }}
+    }
+}
