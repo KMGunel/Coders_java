a72f846a6d9f458552df4675f90913b8c64c464c
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-05-22 16:57:15 +0400

    sade ve ya murekkeb

--- src/main/java/exercise/others/SimpleOrComplexDigit.java
+++ src/main/java/exercise/others/SimpleOrComplexDigit.java
@@ -7,15 +7,16 @@ public class SimpleOrComplexDigit {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
         int m=0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if(n%i==0){
                 m++;
+                break;
             }
         }
         if (n==0){
             System.out.println("Daxil edilən ədəd 0-dır.");
         }
-        else if (m>2){
+        else if (m>0){
             System.out.println("Daxil edilən MÜRƏKKƏB ədəddir.");
         }
         else{
