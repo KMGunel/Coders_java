5e014a05e8a1b6ca73b8674682e7f4ab2bdfdcac
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-05-26 21:56:39 +0400

    loop hundred

--- src/main/java/exercise/others/LoopAHundred.java
+++ src/main/java/exercise/others/LoopAHundred.java
@@ -1,13 +1,11 @@
 package main.java.exercise.others;
 
-
-
 public class LoopAHundred {
     public static void main(String[] args) {
         String str = "";
         while (str.length()!=100) {
              str = str.concat("a");
+            System.out.println(str.length());
         }
-        System.out.println(str);
 }
 }
